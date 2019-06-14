36158e0b3beb8cc46c55a0efa6b6460c733e0e99
Reinaldo Freitas <reinaldo.freitas@vogeltelecom.com>
2019-06-14 17:44:25 -0300

    1

---
+++ src/Entity/Redes/Servico.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity\Redes;
+
+/**
+ * Servico
+ */
+class Servico
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nome;
+
+    /**
+     * @var bool
+     */
+    private $ativo = true;
+
+    /**
+     * @var \DateTime
+     */
+    private $dataCadastro = 'now()';
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome.
+     *
+     * @param string $nome
+     *
+     * @return Servico
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome.
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set ativo.
+     *
+     * @param bool $ativo
+     *
+     * @return Servico
+     */
+    public function setAtivo($ativo)
+    {
+        $this->ativo = $ativo;
+
+        return $this;
+    }
+
+    /**
+     * Get ativo.
+     *
+     * @return bool
+     */
+    public function getAtivo()
+    {
+        return $this->ativo;
+    }
+
+    /**
+     * Set dataCadastro.
+     *
+     * @param \DateTime $dataCadastro
+     *
+     * @return Servico
+     */
+    public function setDataCadastro($dataCadastro)
+    {
+        $this->dataCadastro = $dataCadastro;
+
+        return $this;
+    }
+
+    /**
+     * Get dataCadastro.
+     *
+     * @return \DateTime
+     */
+    public function getDataCadastro()
+    {
+        return $this->dataCadastro;
+    }
+}
